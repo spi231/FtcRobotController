18b8d48ad45463f6ae94eea2ce504462cfb02659
spi231 <maxkranc08@gmail.com>
2025-10-14 09:18:06 -0400

    Removed unnecessary code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Central Disc Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Central Disc Control.java
@@ -18,10 +18,6 @@ public class MecanumDrive extends LinearOpMode {
 
     // variables
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null; // W1
-    private DcMotor leftBackDrive = null; // W4
-    private DcMotor rightFrontDrive = null; // W2
-    private DcMotor rightBackDrive = null; // W3
     private DcMotor armTilt2 = null;
     private DcMotor armScissor = null;
     private DcMotor armTilt = null;
@@ -33,19 +29,10 @@ public class MecanumDrive extends LinearOpMode {
 
         // Initialize the hardware variables. 
         // Names are the same as on the Control Hub.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "frontLeftMotor"); // port 0 / W1
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "frontRightMotor"); //port 1 / W2
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "backLeftMotor"); // port 2 / W4
-        rightBackDrive = hardwareMap.get(DcMotor.class, "backRightMotor"); // port 3 / W3
         armTilt2 = hardwareMap.get(DcMotor.class, "armTilt2"); //expansion 0
         armTilt = hardwareMap.get(DcMotor.class, "armTilt"); //expansion 2
         armScissor = hardwareMap.get(DcMotor.class, "armScissor"); //expansion 1
         armSpin = hardwareMap.get(CRServo.class, "armSpin"); //servo port 0
-        //Directions for the motors
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
         //Reset scissor encoders for the code-stop at extension over 40 cm
         armScissor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armScissor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
@@ -58,43 +45,11 @@ public class MecanumDrive extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double max;
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  -gamepad1.right_stick_x;
             //Throttle for fine movement, extension for the code-lock
             double armExtension= armScissor.getCurrentPosition();
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = (axial + lateral + yaw);
-            double rightFrontPower = (axial - lateral - yaw);
-            double leftBackPower   = (axial - lateral + yaw);
-            double rightBackPower  = (axial + lateral - yaw);
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
-            }
-
-            // Send calculated power to wheels
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.addData("Arm extend", armScissor.getCurrentPosition());
             telemetry.update();
         //climber code
